f6cbe4ab9d517313d0ffd62b9d3d8c29c1e4a5f8
Miruna Arcaleanu <mirunaac@gmail.com>
2021-03-11 19:47:44 +0200

    jquery branch

--- resources/main.js
+++ resources/main.js
@@ -1,49 +1,4 @@
 'use strict'
-// document.addEventListener("DOMContentLoaded", function(event){
-//     const menuItems= document.getElementsByClassName('menu-item');
-
-//     for(let i=0; i<menuItems.length; i++){
-//         const menuItem= menuItems[i];
-    
-//         menuItem.addEventListener('click' , function(){
-//             for(let j=0; j<menuItems.length;j++){
-//                 if(menuItems[j].classList.contains('selected')){
-//                     menuItems[j].classList.remove('selected');
-//                 }else{
-//                     continue;
-//                 }
-//             }
-//             menuItem.classList.add('selected');
-//             console.log('clicked');
-
-//         const dataContent= menuItem.dataset.content;
-//         const contentElements= document.getElementsByClassName('content');
-//         for(let k=0; k<contentElements.length;k++){
-//             if(contentElements[k].classList.contains('hidden')) {
-//                 continue;
-//             } else {
-//                 contentElements[k].classList.add('hidden');
-//             }
-//         }
-//         document.getElementById(dataContent).classList.remove("hidden");
-//         console.log('page changed');
-
-
-//         const logoElements= document.getElementsByClassName('inner-page-logo');
-//         for(let l=0;l<logoElements.length;l++){
-//             if(logoElements[l].classList.contains('hidden')){
-//                 continue;
-//             }else{
-//                 logoElements[l].classList.add('hidden');
-//             }
-//         }
-//         document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
-//         console.log('logo changed');
-
-//         });
-//     }
-// });
-
 $(function() {
     const menuItems = $('main').find('.menu-item');
     const pageContent= $('.content-wrappers').find('.content');
