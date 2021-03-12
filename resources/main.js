baeaf0d4bbf3446c7c06a33667ff2dea268b379e
Miruna Arcaleanu <mirunaac@gmail.com>
2021-03-12 21:10:59 +0200

    update code

--- resources/main.js
+++ resources/main.js
@@ -18,5 +18,32 @@ $(function() {
         logoContent.addClass('hidden');
         $(`.${$(this).data('content')}`).removeClass('hidden');
     }); 
-   
+
+
+    const locationsWrapper= $('.content-wrappers').find('.locations-wrapper');
+    $('#load-button').click(function(){
+        locationsWrapper.text('Loading...');
+
+        $.ajax({
+            type:'GET',
+            url: "https://api.meetup.com/2/cities",
+            
+            success:function(data) {
+                locationsWrapper.html('');
+                for(let i = 0; i < 20; i++) {
+                    let result = data.results[i];
+                    console.log('result: ', result);
+                    let location= `<div class="city-store-location">${result.city} Store in 
+                                        <span class="country-store-location">${result.localized_country_name}</span>
+                                    </div>`;
+                    locationsWrapper.append(location);
+                }
+            },
+
+            error:function(){
+            locationsWrapper.text('There was an error in reading the locations. Please come back later.');
+            },
+            dataType: 'jsonp'
+        });
+    });
 });
